fc3d3786573bf989070286971727eb3c6b8b5a80
Kelvin Bush <kelybush@gmail.com>
2022-07-13 17:21:14 +0300

    add linters

--- spec/morse_ruby_spec.rb
+++ spec/morse_ruby_spec.rb
@@ -1,11 +1,9 @@
-# frozen_string_literal: true
-
 RSpec.describe MorseRuby do
-  it "has a version number" do
+  it 'has a version number' do
     expect(MorseRuby::VERSION).not_to be nil
   end
 
-  it "does something useful" do
+  it 'does something useful' do
     expect(false).to eq(true)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,8 @@
-# frozen_string_literal: true
-
-require "morse_ruby"
+require 'morse_ruby'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
